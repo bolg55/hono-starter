6bdeb8c4ed77f3695d8dfc0132828acb5597862d
Kellen <bolger.kellen@gmail.com>
2025-02-15 18:15:02 -0500

    chore: create default not found and error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
+import notFound from './middlewares/not-found.js'
+import onError from './middlewares/on-error.js'
 
 const app = new OpenAPIHono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.notFound(notFound)
+app.onError(onError)
 
 export default app
---
+++ src/middlewares/index.ts
@@ -0,0 +1,2 @@
+export { default as notFound } from './not-found.js'
+export { default as onError } from './on-error.js'
