52006135a95ad7e187f44af628500625159421a2
Kellen <bolger.kellen@gmail.com>
2025-02-16 14:32:47 -0500

    chore export env types

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
+import type { ZodError } from 'zod'
+
 import { config } from 'dotenv'
 import { expand } from 'dotenv-expand'
 import { z } from 'zod'
@@ -10,7 +12,20 @@ const EnvSchema = z.object({
   LOG_LEVEL: z.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 })
 
-// eslint-disable-next-line node/prefer-global/process
-const env = EnvSchema.parse(process.env)
+export type Env = z.infer<typeof EnvSchema>
+
+// eslint-disable-next-line import/no-mutable-exports
+let env: Env
+
+try {
+// eslint-disable-next-line node/no-process-env
+  env = EnvSchema.parse(process.env)
+}
+catch (e) {
+  const error = e as ZodError
+  console.error('❌ Invalid environment variables:')
+  console.error(error.flatten().fieldErrors)
+  process.exit(1)
+}
 
 export default env
