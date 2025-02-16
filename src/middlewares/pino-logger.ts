74e170720d0288c478d3078df9eb6e1748e59201
Kellen <bolger.kellen@gmail.com>
2025-02-16 14:09:00 -0500

    chore: configure logger

--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -1,11 +1,29 @@
+import env from '@/env.js'
 import { pinoLogger } from 'hono-pino'
-import pretty from 'pino-pretty'
+
+const { NODE_ENV, LOG_LEVEL } = env
 
 export function logger() {
   return pinoLogger({
-    pino: process.env.NODE_ENV === 'production' ? undefined : pretty(),
+    pino: {
+      level: LOG_LEVEL || 'info',
+      transport: NODE_ENV === 'production'
+        ? undefined
+        : {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              levelFirst: true,
+              translateTime: 'SYS:standard',
+              ignore: 'pid,hostname',
+              messageFormat: '{msg}',
+              timestampKey: 'time',
+            },
+          },
+    },
     http: {
       reqId: () => crypto.randomUUID(),
     },
+
   })
 }
