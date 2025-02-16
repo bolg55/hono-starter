1b56666032929c10357e1592f0be41bc48bf777d
Kellen <bolger.kellen@gmail.com>
2025-02-16 15:03:31 -0500

    style: disable strict mode

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -8,7 +8,9 @@ import serveEmojiFavicon from '@/middlewares/serve-emoji-favicon.js'
 import type { AppBindings } from './types.js'
 
 export default function createApp() {
-  const app = new OpenAPIHono<AppBindings>()
+  const app = new OpenAPIHono<AppBindings>({
+    strict: false,
+  })
 
   app.use(serveEmojiFavicon('🔥'))
   app.use(logger())
