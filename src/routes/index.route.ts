5b9a5423bb7df5471fb5f4ec3c2707f84cb6b0c8
Kellen <bolger.kellen@gmail.com>
2025-02-16 19:24:36 -0500

    feat: build openapi zod schemas

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,24 +1,22 @@
 import { createRoute, z } from '@hono/zod-openapi'
 
 import { createRouter } from '@/lib/create-app.js'
+import * as HttpStatusCodes from '@/lib/http-status-codes.js'
+import jsonContent from '@/openapi/helpers/json-content.js'
 
 const router = createRouter().openapi(createRoute({
   method: 'get',
   path: '/',
   responses: {
-    200: {
-      content: {
-        'application/json': {
-          schema: z.object({
-            message: z.string(),
-          }),
-        },
-      },
-      description: 'Success',
-    },
+    [HttpStatusCodes.OK]: jsonContent(
+      z.object({
+        message: z.string(),
+      }),
+      'Hono Starter API Index',
+    ),
   },
 }), (c) => {
-  return c.json({ message: 'Hello World!' })
+  return c.json({ message: 'Hello World!' }, HttpStatusCodes.OK)
 })
 
 export default router
