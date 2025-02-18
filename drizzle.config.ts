2a67acd0b5f44c63429ac10289faff9befb997cc
Kellen <bolger.kellen@gmail.com>
2025-02-17 20:12:56 -0500

    fix: update schema entry point

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ const { DATABASE_URL, DATABASE_AUTH_TOKEN } = env
 
 export default defineConfig({
   out: './src/db/migrations',
-  schema: './src/db/schema.ts',
+  schema: './src/db/schema/*.schema.ts',
   dialect: 'turso',
   dbCredentials: {
     url: DATABASE_URL,
